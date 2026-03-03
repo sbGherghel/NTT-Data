375264ca16346d5d4b1dd11cb8e9e7327aa13a46
sbGherghel <sb.gherghel@gmail.com>
2026-03-03 14:28:49 +0200

    Add files via upload
    Assignment 1

---
+++ Petv3/src/main/java/org/example/petapi/Pet.java
@@ -0,0 +1,61 @@
+package org.example.petapi;
+
+public class Pet {
+    private String name;
+    private String owner;
+    private String type;
+    private String race;
+    private int realAge;
+
+    //Constructor
+    public Pet(String name, String owner, String type, String race, int realAge){
+        this.name = name;
+        this.owner = owner;
+        this.race = race;
+        this.realAge = realAge;
+    }
+
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public String getOwner(){
+        return owner;
+    }
+    public String getType(){
+        return type;
+    }
+    public String getRace(){
+        return race;
+    }
+    public int getRealAge(){
+        return realAge;
+    }
+
+    //calculate human age
+    public int getHumanAge(){
+        if("dig".equalsIgnoreCase(this.type)){
+            return this.realAge*7;
+        } else if ("cat".equalsIgnoreCase(this.type)){
+            return this.realAge*6;
+        }
+        return this.realAge*5;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setRace(String race) {
+        this.race = race;
+    }
+    public void setRealAge(int realAge) {
+        this.realAge = realAge;
+    }
+}
---
+++ Petv3/src/main/java/org/example/petapi/Petv3Application.java
@@ -0,0 +1,13 @@
+package org.example.petapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Petv3Application.class, args);
+    }
+
+}
